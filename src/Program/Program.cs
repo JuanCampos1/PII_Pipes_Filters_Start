97bf81401a7c822cf0f067e6d1203f1b9cdfbb8a
[Juan Campos] <[juan.campos@correo.ucu.edu.uy]>
2021-10-19 17:38:13 -0300

    ejercicios-resueltos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -8,6 +9,31 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            //Ejercicio 1
+            PictureProvider pictureProvider = new PictureProvider();
+            IPicture picture = pictureProvider.GetPicture(@"beer.jpg");
+            PipeNull pipenull = new PipeNull();
+            FilterNegative negative = new FilterNegative();
+            PipeSerial serial_1 = new PipeSerial(negative, pipenull);
+            IPicture negativefilter = serial_1.Send(picture);
+            FilterGreyscale grey = new FilterGreyscale();
+            PipeSerial serial_2 = new PipeSerial(grey, serial_1);
+            IPicture greyfilter = serial_2.Send(picture);
+            
+            //Ejercicio 2
+            pictureProvider.SavePicture(negativefilter, @"negativefilter.jpg");
+            pictureProvider.SavePicture(greyfilter, @"greyfilter.jpg");
+
+            //Ejercicio 3
+            var twitter = new TwitterImage();
+            Console.WriteLine(twitter.PublishToTwitter("", @"negativefilter.jpg"));
+
+            //Ejercicio 4}
+            FilterTweeter tweet = new FilterTweeter($@"{picture}", "");
+            PipeSerial serial_3 = new PipeSerial(tweet, pipenull);
+            FilterConditional Face = new FilterConditional();
+            PipeSerial SerialTwitter = new PipeSerial(Face, serial_3);
+            PipeConditionalFork pipeConditional = new PipeConditionalFork(SerialTwitter, serial_1);
         }
     }
 }
